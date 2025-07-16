b493464eb6dfbb533006b3c3f5332376bcdbad2d
Mowah Chibuike <mctimmykings@gmail.com>
2025-07-16 17:16:30 +0100

    Start working on the endpoint for getting IP address of the microcontroller

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/devices/{ip}')
+def get_ipaddress(ip):
+    return jsonify({
+        "ip": ip 
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
